8b6b1c428662b75744316c61006acf0be8c48824
Larissa <larissa_rodr@hotmail.com>
2020-04-28 14:42:23 -0500

    Including # of individuals

--- frontend/src/pages/Stocks/index.js
+++ frontend/src/pages/Stocks/index.js
@@ -21,32 +21,38 @@ export default function Stocks(){
       return (
         <>
             <Header/>
-            <table className="flatTable">
-                <thead>
-                    <tr className="titleTr">
-                        <th>STOCK ID</th>
-                        <th>NAME</th>
-                        <th>RESPONSIBLE</th>
-                        <th>SETUP DATE</th>
-                        <th>LAST CHECKED BY</th>
-                        <th>LAST CHECK DATE</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {stocks.map(stock => (
-                        <tr className="bodyTr" key={stock.id}>
-                            <td>{stock.id}</td>
-                            <td>{stock.name}</td>
-                            <td>{stock.responsible_user_username}</td>
-                            <td>{Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', 
-                                                            minute: 'numeric'}).format(stock.setup_date)}</td>
-                            <td>{stock.last_check_user_username}</td>
-                            <td>{Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', 
-                                                            minute: 'numeric'}).format(stock.last_check_date)}</td>
+            <div className="tableWrapper">
+                <table className="flatTable">
+                    <thead>
+                        <tr className="titleTr">
+                            <th>STOCK ID</th>
+                            <th>NAME</th>
+                            <th>RESPONSIBLE</th>
+                            <th>SETUP DATE</th>
+                            <th># OF INDIVIDUALS</th>
+                            <th>LAST CHECKED BY</th>
+                            <th>LAST CHECK DATE</th>
+                            <th>SEE DETAILS</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {stocks.map(stock => (
+                            <tr className="bodyTr" key={stock.id}>
+                                <td>{stock.id}</td>
+                                <td>{stock.name}</td>
+                                <td>{stock.responsible_user_username}</td>
+                                <td>{Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', 
+                                                                minute: 'numeric'}).format(stock.setup_date)}</td>
+                                <td>{stock.number_of_males + stock.number_of_females + stock.number_of_hermaphrodites + stock.number_of_juveniles}</td>
+                                <td>{stock.last_check_user_username}</td>
+                                <td>{Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', 
+                                                                minute: 'numeric'}).format(stock.last_check_date)}</td>
+                                <td>Click here</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
             <Footer/>
         </>
       )
